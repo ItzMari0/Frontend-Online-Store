0a20216ee99418e123034620d0abc69c60768915
Lucas <lucas_emanuel_sc@hotmail.com>
2022-08-05 16:02:37 -0300

    terminei a 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aquid
+  const result = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const response = await result.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/${categoryId}/${query}`);
+  const response = await result.json();
+  return response;
 }
